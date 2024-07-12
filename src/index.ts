3b38f21cdc0bb22324dc7cb4e36e5c3e7100bb90
yatanvesh <yatan.vesh@gmail.com>
2024-07-12 10:29:48 +0530

    fix: update startup sequence

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const createWindow = (): void => {
 		webPreferences: {
 			preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
 			webviewTag: true,
-			devTools: true,
+			devTools: false,
 		},
 	});
 
@@ -72,7 +72,7 @@ const createWindow = (): void => {
 	void mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -104,18 +104,18 @@ app.on('ready', () => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+	app.quit();
+	// if (process.platform !== 'darwin') {
+	// }
 });
 
-app.on('activate', () => {
+// app.on('activate', () => {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
-	}
-});
+	// if (BrowserWindow.getAllWindows().length === 0) {
+	// 	createWindow();
+	// }
+// });
 
 // app.setLoginItemSettings({
 // 	openAtLogin: true
