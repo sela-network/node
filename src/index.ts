9a419d3c46a6803095a261db1e79b729e3b8a4cb
yatanvesh <yatan.vesh@gmail.com>
2024-07-12 16:53:08 +0530

    chore: update release

--- src/index.ts
+++ src/index.ts
@@ -138,8 +138,8 @@ app.on('window-all-closed', () => {
 	// }
 // });
 
-// app.setLoginItemSettings({
-// 	openAtLogin: true
-// })
+app.setLoginItemSettings({
+	openAtLogin: true
+})
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
