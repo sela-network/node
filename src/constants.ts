4c2d9b238b4ba0fcdb3dc7c80850668d3293d802
yatanvesh <yatan.vesh@gmail.com>
2024-07-12 20:15:18 +0530

    chore: update prod url

--- src/constants.ts
+++ src/constants.ts
@@ -16,12 +16,12 @@ export const HashRoutes = {
 
 export const TON_MANIFEST_URL =
 	'https://snapx-dev-public.s3.ap-northeast-2.amazonaws.com/tonconnect-manifest.json';
-
-export const SERVER_URL = 'https://api.dev.snapx.co';
-// export const SERVER_URL = 'https://api.snapx.co';
+export const SERVER_URL = 'https://api.snapx.co';
+export const SNAPX_URL = 'https://t.me/snapx_prod_bot/snapx_app';
+// export const SERVER_URL = 'https://api.dev.snapx.co';
 // export const SERVER_URL = 'http://localhost:8003';
 export const UPTIME_UPDATE_INTERVAL_IN_MS = 5000;
 export const REWARD_UPDATE_INTERVAL_IN_MS = 5000;
-export const SNAPX_URL = 'https://t.me/snapx_staging_bot/snapx_staging';
+// export const SNAPX_URL = 'https://t.me/snapx_staging_bot/snapx_staging';
 export const NODE_APP_SHARE_LINK = 'https://snapx.co/#NodeApp'
 export const SENTRY_DSN = 'https://78a7dc645d91b03605f5ed8d6c345f27@o4507524057268224.ingest.us.sentry.io/4507587896475648';
